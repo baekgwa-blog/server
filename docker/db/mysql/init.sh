4226f1a504eeee058de92a88c41b66a6cd9ad04b
KangBaekGwa <ksu9801@gmail.com>
2025-05-30 23:55:06 +0900

    fix : db 생성 compose 변경 - exporter 사용 안함으로 삭제 - 필요없는 주석 삭제 - 접근용 사용자 sh 추가 - 환경변수, 공통으로 사용하도록 네이밍 통일 (.env)

--- docker/db/mysql/init.sh
+++ docker/db/mysql/init.sh
@@ -6,14 +6,14 @@ mysql -u root -p${MYSQL_ROOT_PASSWORD} <<EOF
 CREATE DATABASE IF NOT EXISTS baekgwa_blog;
 USE baekgwa_blog;
 
-# Exporter 사용자 생성
-CREATE USER IF NOT EXISTS '${EXPORTER_NAME}'@'%' IDENTIFIED BY '${EXPORTER_PASSWORD}' WITH MAX_USER_CONNECTIONS 3;
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO '${EXPORTER_NAME}'@'%';
+-- 사용자 생성 및 권한 부여
+CREATE USER IF NOT EXISTS '${RDBMS_USERNAME}'@'%' IDENTIFIED BY '${RDBMS_PASSWORD}';
+GRANT ALL PRIVILEGES ON baekgwa_blog.* TO '${RDBMS_USERNAME}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
 # 로그: 쿼리 실행 후
-echo "$(date '+%Y-%m-%d %H:%M:%S') - 데이터베이스와 exporter 사용자 생성 완료"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 데이터베이스 및 사용자 생성 완료"
 
 # 스크립트 종료 로그
 echo "$(date '+%Y-%m-%d %H:%M:%S') - MySQL 초기화 완료"
